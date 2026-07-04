b6aacdfa4ead73877d1581dfbe97a6493f48e6d4
Muhammad Nur Muliadi <nurmuliadi024@gmail.com>
2026-07-04 20:36:42 +0800

    Add: check-completeness-of-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/check-completeness-of-a-binary-tree/submission-3.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        deq = deque([root])
+        complete = False
+        while deq:
+            node = deq.popleft()
+
+            if node.left:
+                deq.append(node.left)
+            if node.right:
+                deq.append(node.right)
+            
+            if not node.left and node.right:
+                return False
+            elif not node.left and not node.right:
+                complete = True
+
+            if complete and node.left and node.right:
+                return False
+            elif complete and node.left and not node.right:
+                return False
+        return True
+            
+            
